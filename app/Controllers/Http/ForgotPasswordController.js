be7d0291dbfded357a40283b141cd91258b55802
Davi <davirodrigues8@gmail.com>
2019-06-16 20:21:53 -0300

    adds forgot password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -3,6 +3,7 @@
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
+const moment = use('moment')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -34,6 +35,34 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo não deu certo. Este email existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de recuperação está expirado' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Algo deu errado ao resetar a sua senha' } })
     }
   }
 }
